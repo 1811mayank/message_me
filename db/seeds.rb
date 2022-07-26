af8de6981c409744975fdee10e66dba870cdecfe
Mayank <mayanksolanki1981@gmail.com>
2022-07-26 15:01:05 +0530

    user model and hirb use

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "mayank", password: "password")
+User.create(username: "solanki", password: "password")
+User.create(username: "ritik", password: "password")
+User.create(username: "ravindra", password: "password")
+User.create(username: "rushikesh", password: "password")
